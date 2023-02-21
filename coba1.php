f6adad2743488676312c12afc731dace3c605931
DimasAthaHandria <dimashandria07@gmail.com>
2023-02-21 15:02:31 +0700

    Hii

---
+++ coba1.php
@@ -0,0 +1,9 @@
+<?php
+    define('PHI', 3.14);
+    define('DENAME,' 'inventori');
+    define('DBSERVER', 'localhost');
+
+    echo 'PHI :'.PHI;
+    echo 'Nama Database :'.DBNAME;
+    echo 'Lokasi Database:'.DBSERVER;
+?>
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+    $nama = 'Dimas Handria';
+    $umur = 19;
+    $berat = 50;
+
+    echo 'Nama :' .$nama;
+    echo '<br> Umur :' .$umur;
+    echo '<br> Berat :' .$berat;
+?>
